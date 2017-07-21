14cd2e6b9dafb3b4e9835d5f8f84e8deb539e115
Tom Young <tom.young.satx@gmail.com>
2017-07-21 15:10:07 -0500

    index changes

--- app/views/index.php
+++ app/views/index.php
@@ -113,7 +113,7 @@
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2">
                 <h2>Guide Section</h2>
-                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+                <p>There will be some kind of carousel or index of guides here.</p>
             </div>
         </div>
     </section>
@@ -124,7 +124,7 @@
             <div class="container">
                 <div class="col-lg-8 col-lg-offset-2">
                     <h2>Tour Section</h2>
-                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                    <p>Same kind of setup as the guide section.</p>
                 </div>
             </div>
         </div>
